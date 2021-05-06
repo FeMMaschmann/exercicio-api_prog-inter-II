69770daf6e34af26734641419c2169221eff823a
edinhograno <granomotorista@gmail.com>
2021-05-06 10:15:53 -0300

    Primeiro commit a gente nunca esquece, brincadeirinha

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+// import { Link as LinkR } from 'react-router-dom';
+// import { Link as LinkS} from 'react-scroll';
+ 
+// -mt-20 margin-top: -80px para o nav
+export const Nav = styled.nav`
+    font-size: 18px;
+`;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+// import { FaBars } from "react-icons/fa";
+import {
+  Nav
+} from "./NavbarElements";
+// import { animateScroll as scroll } from "react-scroll";
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+            Oi
+            </Nav>
+        </>
+    );
+};
+export default Navbar;
